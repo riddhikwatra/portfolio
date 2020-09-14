d7e04c2c02abb2bf926fc8cc338cee869c368093
riddhikwatra <riddhikwatra17@gmail.com>
2020-09-14 23:01:30 +0530

    updated sql file

--- portfolio.sql
+++ portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2020 at 09:14 AM
+-- Generation Time: Sep 14, 2020 at 07:28 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -25,6 +25,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `course` varchar(30) NOT NULL,
+  `institute` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `education`
+--
+
+INSERT INTO `education` (`course`, `institute`, `user_id`) VALUES
+('BSC', 'DU', 29),
+('MCA', 'IGDTUW', 29);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience`
+--
+
+CREATE TABLE `experience` (
+  `job` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `experience`
+--
+
+INSERT INTO `experience` (`job`, `user_id`) VALUES
+('worked at 2', 29),
+('worked at 1', 29);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `images`
 --
 
@@ -36,6 +75,13 @@ CREATE TABLE `images` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `file_name`, `uploaded_on`, `status`, `user_id`) VALUES
+(1, 'meme1.png', '2020-09-14 22:54:25', '1', 29);
+
 -- --------------------------------------------------------
 
 --
@@ -45,12 +91,35 @@ CREATE TABLE `images` (
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `intro` text NOT NULL
+  `intro` text NOT NULL,
+  `facebook` varchar(100) NOT NULL,
+  `twitter` varchar(100) NOT NULL,
+  `instagram` varchar(100) NOT NULL,
+  `linkedin` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `intro`, `facebook`, `twitter`, `instagram`, `linkedin`) VALUES
+(29, 'Riddhi', 'hey guysssdasdsf', 'https://www.facebook.com/riddhi.kwatra/', 'https://twitter.com/KwatraRiddhi?s=09', 'https://www.instagram.com/riddhi_kwatra/?hl=en', 'https://www.linkedin.com/in/riddhi-kwatra-5aa1b4192');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `experience`
+--
+ALTER TABLE `experience`
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `images`
@@ -73,17 +142,29 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `education`
+--
+ALTER TABLE `education`
+  ADD CONSTRAINT `education_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `experience`
+--
+ALTER TABLE `experience`
+  ADD CONSTRAINT `experience_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `images`
