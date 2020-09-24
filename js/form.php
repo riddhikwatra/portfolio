35cafe145cf8936d34b51ed149ae02aa1eb533f2
riddhikwatra <riddhikwatra17@gmail.com>
2020-09-24 14:14:09 +0530

    changed layout

--- js/form.php
+++ js/form.php
@@ -6,71 +6,123 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/form.css">
-    <title>form</title>
+    <title>sign up/sign in</title>
 </head>
 <body>
+  <div class="split left">
     <form class="form" action="website.php" method="post" enctype="multipart/form-data">
-    <div class="form-control">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name">
-        <br>
-        <label for="image">Image</label>
-        <input type="file" name="files[]" multiple>
-        <br>
-        <label for="intro">Introduction</label>
-        <textarea name="intro" rows="8" cols="80"></textarea>
-        <br>
-        <label for="education">Education</label>
-        <div class="ed" id="id">
-            <input type="text" name="edH0" value="Course">
-            <br>
-            <input type="text" name="ed0" value="Institution">
-            <span id="responceEd"></span>
-            <input class="btn" type="button" onclick="addInputEd()" value="click to add more education details"/>
+    <h1 style="color:white">Sign Up!!</h1>
+      <div class="form-control">
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name">
+          <br>
+          <label for="email">Email Address</label>
+          <input type="text" name="email" placeholder="email">
+          <br>
+          <label for="password">Password</label>
+          <input type="password" name="pass" placeholder="***">
+          <br>
+          <label for="profile">Profile Picture</label>
+          <input type="file" name="profile" style="height:30px">
+          <br>
+          <label for="image">Images</label>
+          <input type="file" name="files[]" multiple style="height:30px">
+          <br>
+          <label for="intro">Introduction</label>
+          <textarea name="intro" rows="8" cols="80"></textarea>
+          <br>
+          <label for="education">Education</label>
+          <div class="ed" id="id">
+              <input type="text" name="edH0" placeholder="Course">
+              <br>
+              <input type="text" name="ed0" placeholder="Institution">
+              <span id="responceEd"></span>
+              <input class="btn" type="button" onclick="addInputEd()" style="background-color:grey;color:pink; height:35px" value="click to add more education details"/>
+          </div>
+          <label for="experience">Experience</label>
+          <div class="ex" id="ex">
+              <input type="text" name="exp0" placeholder="Job">
+              <br>
+              <span id="responceExp"></span>
+              <input class="btn" type="button" onclick="addInputExp()" style="background-color:grey;color:pink; height:35px" value="click to add more experience details"/>
+          </div>
+          <label for="projects">Projects</label>
+          <div class="pro" id="pro">
+              <input type="text" name="proH0" placeholder="project name">
+              <br>
+              <textarea name="pro0" placeholder="Description" rows="4" cols="80"></textarea>
+              <span id="responcePro"></span>
+              <input class="btn" type="button" onclick="addInputPro()" style="background-color:grey;color:pink; height:35px" value="click to add more project details"/>
+          </div>
+          <input type="hidden" name="countEd" id="countEd" value="1">
+          <input type="hidden" name="countExp" id="countExp" value="1">
+          <input type="hidden" name="countPro" id="countPro" value="1">
+          <label for="facebook">Link To Facebook Account</label>
+          <input type="text" name="fb" id="fb">
+          <br>
+          <label for="twitter">Link To Twitter Account</label>
+          <input type="text" name="twitter" id="twitter">
+          <br>
+          <label for="instagram">Link To Instagram Account</label>
+          <input type="text" name="insta" id="insta">
+          <br>
+          <label for="linkedin">Link to Your LinkedIn Account</label>
+          <input type="text" name="link" id="link">
+          <br>
+          <button class="btn" type="submit" name="submit">submit</button>
+          <br>
+          <br>
+          <br>
         </div>
-        <input type="hidden" name="countEd" id="count">
-        <label for="experience">Experience</label>
-        <div class="ex" id="ex">
-            <input type="text" name="exp0" value="job">
-            <br>
-            <span id="responceExp"></span>
-            <input class="btn" type="button" onclick="addInputExp()" value="click to add more experience details"/>
-        </div>
-        <input type="hidden" name="countEd" id="countEd" value="1">
-        <input type="hidden" name="countExp" id="countExp" value="1">
-        <label for="facebook">link to facebook profile</label>
-        <input type="text" name="fb" id="fb">
+      </form>
+  <div class="centered">
+  </div>
+</div>
+
+<div class="split right">
+  <div class="centered">
+    <form class="form" action="website.php" method="post" enctype="multipart/form-data">
+      <h1 style="color:white">Sign In!!</h1>
+      <div class="form-control">
+        <label for="email">Email Address</label>
+        <input type="text" name="email" placeholder="email">
         <br>
-        <label for="twitter">link to twitter account</label>
-        <input type="text" name="twitter" id="twitter">
+        <label for="password">Password</label>
+        <input type="password" name="pass" placeholder="***">
         <br>
-        <label for="instagram">link to instaram account</label>
-        <input type="text" name="insta" id="insta">
-        <br>
-        <label for="linkedin">link to your linkedIn account</label>
-        <input type="text" name="link" id="link">
-        <br>
-        <button class="btn" type="submit" name="submit">submit</button>
+        <button class="btn" type="submit" name="submit2">Sign In</button>
       </div>
     </form>
+  </div>
+</div>
+
 </body>
 </html>
 <script>
 var countEds =1;
 var countExps =1;
+var countPros =1;
 function addInputEd()
 {
      var course= "edH"+countEds;
      var institute="ed"+countEds;
-     document.getElementById('responceEd').innerHTML+='<br/><input type="text" value="Course" id="'+course+'" name="'+course+'" /><br/><input type="text" value="Institute" id="'+institute+'" name="'+institute+'" /><br/>';
+     document.getElementById('responceEd').innerHTML+='<br/><input type="text" placeholder="Course" id="'+course+'" name="'+course+'" /><br/><input type="text" placeholder="Institute" id="'+institute+'" name="'+institute+'" /><br/>';
      countEds += 1;
      document.getElementById('countEd').value=countEds;
 }
 function addInputExp()
 {
      var job= "exp"+countExps;
-     document.getElementById('responceExp').innerHTML+='<br/><input type="text" value="Job" id="'+job+'" name="'+job+'" /><br/>';
+     document.getElementById('responceExp').innerHTML+='<br/><input type="text" placeholder="Job" id="'+job+'" name="'+job+'" /><br/>';
      countExps += 1;
      document.getElementById('countExp').value=countExps;
+}
+function addInputPro()
+{
+     var topic= "proH"+countPros;
+     var desc="pro"+countPros;
+     document.getElementById('responcePro').innerHTML+='<br/><input type="text" placeholder="project name" id="'+topic+'" name="'+topic+'" /><br/><textarea name="'+desc+'" placeholder="Description" rows="4" cols="80"></textarea><br/>';
+     countPros += 1;
+     document.getElementById('countPro').value=countPros;
 }
 </script>
--- js/website.php
+++ js/website.php
@@ -4,63 +4,81 @@
 if(isset($_POST['submit'])){
     // File upload configuration
     $name=$_POST['name'];
+    $email=$_POST['email'];
+    $pass=$_POST['pass'];
     $intro=$_POST['intro'];
     $countEd=$_POST['countEd'];
     $countExp=$_POST['countExp'];
+    $countPro=$_POST['countPro'];
     $fb=$_POST['fb'];
     $twitter=$_POST['twitter'];
     $insta=$_POST['insta'];
     $link=$_POST['link'];
-    echo "$countEd";
-    echo "$countExp";
-   $insert = $db->query("INSERT INTO users(name,intro,facebook,twitter,instagram,linkedin)  VALUES ('$name','$intro','$fb','$twitter','$insta','$link')");
+
+   $insert = $db->query("INSERT INTO users(name,email,password,intro,facebook,twitter,instagram,linkedin)  VALUES ('$name','$email','$pass','$intro','$fb','$twitter','$insta','$link')");
    if(!$insert)
    echo "there was a problem";
    else{
      $query = $db->query("SELECT max(user_id) as id FROM users");
      $row = mysqli_fetch_array($query);
      $user_id=$row['id'];
-  }
+     $baseName = basename($_FILES['profile']['name']);
+     $path=pathinfo($baseName);
+     $fileName=$path['filename'];
+     $extension = $path['extension'];
+     $file = $fileName.(string)$user_id.".".$extension;
+     $update = $db->query("UPDATE users set profile='$file' where user_id =$user_id");
+     if(!$update){
+       echo "there was an issue";
+     }
+     $targetFilePath="uploads/".$file;
+     if(isset($_FILES["profile"])){
+     if(!move_uploaded_file($_FILES["profile"]["tmp_name"],$targetFilePath)){
+       echo "never mind";
+     }
+    }
+   }
     $targetDir = "uploads/";
     $allowTypes = array('jpg','png','jpeg','gif');
-    $insertValuesSQL=$errorUpload=$errorUploadType="";
+    $insertValuesSQL=$errorUpload=$errorUploadType=$statusMsg="";
     $fileNames = array_filter($_FILES['files']['name']);
     if(!empty($fileNames)){
         foreach($_FILES['files']['name'] as $key=>$val){
             // File upload path
-            $fileName = basename($_FILES['files']['name'][$key]);
-            $targetFilePath = $targetDir . $fileName;
+            $baseName = basename($_FILES['files']['name'][$key]);
+            $path=pathinfo($baseName);
+            $fileName=$path['filename'];
+            $extension = $path['extension'];
+            $file = $fileName.(string)$user_id.".".$extension;
+              $targetFilePath = $targetDir.$fileName.(string)$user_id.".".$extension;
 
-            // Check whether file type is valid
-            $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
-            if(in_array($fileType, $allowTypes)){
+              // Check whether file type is valid
+            if(in_array($extension, $allowTypes)){
                 // Upload file to server
                 if(move_uploaded_file($_FILES["files"]["tmp_name"][$key], $targetFilePath)){
                     // Image db insert sql
-                    $insertValuesSQL .= "('".$fileName."', NOW(),$user_id),";
+                    $insertValuesSQL .= "('".$file."',$user_id),";
                 }else{
                     $errorUpload .= $_FILES['files']['name'][$key].' | ';
                 }
             }else{
                 $errorUploadType .= $_FILES['files']['name'][$key].' | ';
             }
+          }
         }
 
         if(!empty($insertValuesSQL)){
-            $insertValuesSQL = trim($insertValuesSQL, ',');
             // Insert image file name into database
-            $insert = $db->query("INSERT INTO images (file_name, uploaded_on,user_id) VALUES $insertValuesSQL");
-            if($insert){
-                $statusMsg = "Files are uploaded successfully.";
-            }else{
+            $insertValuesSQL=trim($insertValuesSQL,',');
+            $insert = $db->query("INSERT INTO images (file_name,user_id) VALUES $insertValuesSQL");
+            if(!$insert){
                 $statusMsg = "Sorry, there was an error uploading your file.";
             }
         }
-    }else{
+    else{
         $statusMsg = 'Please select a file to upload.';
     }
 
-    // Display status message
     while($countEd>0){
       $course=$_POST["edH".(string)($countEd-1)];
       $institute=$_POST["ed".(string)($countEd-1)];
@@ -78,23 +96,263 @@ if(isset($_POST['submit'])){
       }
       $countExp=$countExp-1;
     }
+    while($countPro>0){
+      $topic=$_POST["proH".(string)($countPro-1)];
+      $desc=$_POST["pro".(string)($countPro-1)];
+      $insert = $db->query("INSERT INTO project VALUES ('$topic','$desc',$user_id)");
+      if(!$insert){
+          $statusMsg = "Sorry, there was an error uploading your file.";
+      }
+      $countPro=$countPro-1;
+    }
+    if($statusMsg!="")
     echo $statusMsg;
-
 }
+
+if(isset($_POST['submit2'])){
+  echo "string";
+  $email=$_POST['email'];
+  $pass=$_POST['pass'];
+  $query = $db->query("SELECT user_id  as id FROM users where email='$email' and password='$pass'");
+  $row = mysqli_fetch_array($query);
+  $user_id=$row['id'];
+}
+
+?>
+<?php
+  $id=$user_id;
+  ($db = mysqli_connect('localhost','root','','portfolio'))or die("connection failed");
+  $query = $db->query("select * from users where user_id=$id");
+  $row = mysqli_fetch_array($query);
+  $name=$row["name"];
+  $profile=$row["profile"];
+  $intro=$row["intro"];
+  $fb=$row["facebook"];
+  $twitter=$row["twitter"];
+  $insta=$row["instagram"];
+  $link=$row["linkedin"];
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
-      <link rel="stylesheet" href="../css/form.css">
+    <title><?php  echo"$name" ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   </head>
   <body>
-    <form class="form" action="new.php" method="post">
-        <div class="form-control">
-          <input type="hidden" name="id" value="<?php echo $user_id ?>">
-            <button class="btn" type="submit" name="submit">submit</button>
-         </div>
-    </form>
-  </body>
-</html>
+        <header class="main-header" id="header">
+          <nav class=" main-header-nav" id="navbar">
+                  <!--  <img class="logo" src="/me.jpg" alt="Riddhi Kwatra">-->
+            <ul class="main-header-item-list collapse navbar-collapse ">
+                <li class="main-header-item">
+                    <a href="#introduction" class="smoothScroll">Introduction</a>
+                </li>
+                <li class="main-header-item">
+                    <a href="#education" class="smoothScroll">Education</a>
+                </li>
+                <li class="main-header-item">
+                    <a href="#experience" class="smoothScroll">Experience</a>
+                </li>
+  							<li class="main-header-item">
+    								<a href="#projects" class="smoothScroll">Projects</a>
+								</li>
+								<li class="main-header-item">
+  									<a href="#gallery" class="smoothScroll">Gallery</a>
+  						  </li>
+                <li class="main-header-item">
+                    <a href="#contact" class="smoothScroll">Contact</a>
+                </li>
+            </ul>
+          </nav>
+        </header>
+        <div class="introsec">
+            <!--- Image Slider -->
+        		<img src="uploads/<?php echo "$profile"; ?>" alt="" class="picture">
+            <div class= "intro">
+                    <h1 class="name">Hey!</h1>
+        						<h1 id="name" class="name"></h1>
+        						<p id="desc" class="font-18"><?php echo "$intro"; ?></p>
+            </div>
+        </div>
+        <!--- Image Slider -->
+
+    <div class="content">
+    <section class="sec" id="education">
+            <br>
+            <br>
+            <h1>Education
+                <i class="material-icons font-34">school</i>
+            </h1>
+            <hr>
+            <?php
+              $query = $db->query("SELECT * FROM education where user_id=$id");
+              if($query->num_rows > 0){
+                while($row = $query->fetch_assoc()){
+             ?>
+                  <h2><?php $c=$row["course"]; echo "$c"; ?>
+                  </h2>
+                  <h3><?php $i=$row['institute']; echo "$i"; ?></h3>
+             <?php
+                }
+              }
+            ?>
+    </section>
+    <section class="sec" id="experience">
+            <br>
+            <br>
+            <h1>Experience
+                <i class="material-icons font-34">work</i>
+            </h1>
+            <hr>
+            <ul>
+              <?php
+              $query = $db->query("SELECT job FROM experience where user_id=$id");
+              if($query->num_rows > 0){
+                while($row = $query->fetch_assoc()){
+               ?>
+                <li class="font-18"><?php $j=$row["job"]; echo "$j"; ?></li>
+               <?php
+               }
+              }
+             ?>
+            </ul>
+    </section>
+    <section class="sec" id="projects">
+            <br>
+            <br>
+            <h1>Projects
+                <i class="material-icons font-34">work</i>
+            </h1>
+            <hr>
+            <?php
+              $query = $db->query("SELECT * FROM project where user_id=$id");
+              if($query->num_rows > 0){
+                while($row = $query->fetch_assoc()){
+             ?>
+                  <h2><?php $c=$row["project"]; echo "$c"; ?>
+                  </h2>
+                  <h3><?php $i=$row['description']; echo "$i"; ?></h3>
+             <?php
+                }
+              }
+            ?>
+    </section>
+    <section class="sec" id="gallery">
+    <br>
+    <br>
+    <h1>Gallery
+    		<i class="material-icons font-34">collections</i>
+    </h1>
+    <hr>
+    <div class="carousel slide carousel-fade slides" data-ride="carousel" data-interval="1500">
+        <div class="carousel-inner">
+          <?php
+          $query = $db->query("SELECT file_name FROM images where user_id=$id ");
+          $count = $query->num_rows;
+          if($query->num_rows > 0){
+            while($row = $query->fetch_assoc()){
+          ?>
+                  <div class="carousel-item <?php if($count==1) echo "active"; ?>">
+          <?php
+
+                      $imageURL = 'uploads/'.$row["file_name"];
+          ?>
+                      <img class="image" src="<?php echo $imageURL; ?>" alt="">
+                      <div class="carousel-cap"></div>
+                  </div>
+          <?php
+                $count=$count-1;
+          }
+        }
+        else{
+          echo "no photos found";
+          }
+           ?>
+           <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    		 	 <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+    </div>
+    </section>
+    <section class="sec" id="contact">
+            <h1>Contact
+                <i class="material-icons font-34">contact_mail</i>
+            </h1>
+            <hr>
+    				<nav class="contact-nav-bar">
+    					<ul class="contact-list">
+    						<li class="contact-item">
+    							<a href="<?php echo $fb ?>"><i class="fab fa-facebook-f contact-icon fa-5x"></i></a>
+    						</li>
+    						<li class="contact-item">
+    							<a href="<?php echo $twitter ?>"><i class="fab fa-twitter contact-icon fa-5x"></i></a>
+    						</li>
+    						<li class="contact-item">
+    							<a href="<?php echo $insta ?>"><i class="fab fa-instagram contact-icon fa-5x"></i></a>
+    						</li>
+    						<li class="contact-item">
+    							<a href="<?php echo $link ?>"><i class="fab fa-linkedin-in contact-icon fa-5x"></i></a>
+    						</li>
+    					</ul>
+    				</nav>
+
+    </section>
+    <a href="form.php">Hello! click here to make your own website like this</a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+          $(window).scroll(function(){
+              $('.picture').css("opacity",1-$(window).scrollTop()/600)
+          })
+      })
+      function hey(){
+        if(i<txt.length){
+          document.getElementById("name").innerHTML += txt.charAt(i);
+          i++;
+          setTimeout(hey, 100);
+        }
+      }
+      var txt="I am ";
+      var i=0;
+      var name="<?php echo $name ?>";
+      txt=txt.concat(name);
+      hey();
+      hey();
+      var slideIndex = 1;
+      var timer = null;
+   showSlides(slideIndex);
+
+   // Next/previous controls
+   function plusSlides(n) {
+      clearTimeout(timer);
+     showSlides(slideIndex += n);
+   }
+
+   // Thumbnail image controls
+   function currentSlide(n) {
+      clearTimeout(timer);
+     showSlides(slideIndex = n);
+   }
+
+   function showSlides(n) {
+     var i;
+     var slides = document.getElementsByClassName("carousel-item");
+     if (n > slides.length) {slideIndex = 1}
+     if (n < 1) {slideIndex = slides.length}
+     for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+     }
+
+     slides[slideIndex-1].style.display = "block";
+   }
+
+    </script>
+
+    </body>
+    </html>
